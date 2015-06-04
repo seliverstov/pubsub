329dc60326547dcd7f7886275dea0a1c0eda5c88
Alexander Seliverstov <a.g.seliverstov@yandex.ru>
2015-06-04 07:59:12 +0000

    Migrate from UberFacts to 4 russian facts accounts

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ var client = new Twitter({
 //var publishJob = schedule.scheduleJob({}, publishTweet);
 //var subscribeJob = schedule.scheduleJob({}, getResentTweets);
 
+var ACCOUNTS = ['5faktov','best_fact','hot_fact','factroom'];
+
+var ACCOUNT = ACCOUNTS[Math.floor(Math.random() * ACCOUNTS.length)];
 
 publishTweet().done(getResentTweets);
 
@@ -26,36 +29,36 @@ if (process.env.DELAY_SEC){
 
 function publishTweet(){
   var d = Q.defer();
-  console.log('It\'s time to publish new tweet');
+  console.log('It\'s time to publish new tweet from account '+ACCOUNT+'');
   MongoClient.connect(process.env.MONGO_URL, function(err, db) {
     if (!err){
-      var tweets = db.collection('tweets');
-      tweets.findOne({status:'R'},{sort:[['_id','asc']]},function(err,tweet){
+      var tweets = db.collection(ACCOUNT);
+      tweets.findOne({status: {$ne:'P'}},{sort:[['_id','asc']]},function(err,tweet){
         if (!err){
           if (tweet){
-            console.log('Tweet '+tweet.id_str+' was choosen for publish');
-            client.post('statuses/update.json', {status: tweet.text_ru.substring(0,140)},  function(err, t, response){
+            console.log('Tweet '+tweet.id_str+' from account '+ACCOUNT+' was choosen for publish');
+            client.post('statuses/update.json', {status: tweet.text},  function(err, t, response){
               if (!err){
-                console.log('Tweet '+tweet.id_str+' was published: ',tweet.text_ru);
+                console.log('Tweet '+tweet.id_str+' from account '+ACCOUNT+' was published: ',tweet.text);
                 tweet.status='P';
                 tweet.published_at = new Date().getTime();
                 tweets.save(tweet,function(err){
                   if (!err) {
-                    console.log('Tweet '+tweet.id_str+' was marked as published');
+                    console.log('Tweet '+tweet.id_str+' from account '+ACCOUNT+' was marked as published');
                   }else{
-                    console.log('Can\'t update tweet '+tweet.id_str+': ',err);
+                    console.log('Can\'t update tweet '+tweet.id_str+' from account '+ACCOUNT+': ',err);
                   }
                   db.close();
                   d.resolve();
                 });
               }else{
-                console.log('Can\'t post tweet '+tweet.id_str+': ',err);
+                console.log('Can\'t post tweet '+tweet.id_str+' from account '+ACCOUNT+': ',err);
                 db.close();
                 d.resolve();
               }
             });
           }else{
-            console.log('Can\'t find any tweet for publish');
+            console.log('Can\'t find any tweet for publish from account '+ACCOUNT);
             db.close();
             d.resolve();
           }
@@ -74,17 +77,26 @@ function publishTweet(){
 }
 
 function getResentTweets(){
-  console.log('It\'s time to check for new tweets');
+  console.log('It\'s time to check for new tweets for account '+ACCOUNT);
 
   var saveTweets = function(tweets){
     MongoClient.connect(process.env.MONGO_URL, function(err, db) {
       if(!err) {
-        var collection = db.collection('tweets');
+        var collection = db.collection(ACCOUNT);
         var promises = [];
         tweets.forEach(function(tweet){
           var d = Q.defer();
           tweet._id = tweet.id_str;
           tweet.status='N';
+          collection.save(tweet,function(err){
+            if (!err) {
+              console.log('Tweet '+tweet.id_str+' from account '+ACCOUNT+' was saved');
+            }else{
+              console.log('Can\'t save tweet '+tweet.id_str+' from account '+ACCOUNT+': ',err);
+            }
+            d.resolve();
+          });
+          /*
           translate(tweet.text, { to: 'ru', key: process.env.YANDEX_TRANSLATE_API_KEY }, function(err, res){
             if (!err){
               tweet.text_ru = res.text[0];
@@ -100,6 +112,7 @@ function getResentTweets(){
               d.resolve();
             });
           });
+          */
           promises.push(d.promise);
         });
         Q.all(promises).done(function(){
@@ -112,13 +125,13 @@ function getResentTweets(){
   };
 
   MongoClient.connect(process.env.MONGO_URL, function(err, db) {
-    var collection = db.collection('tweets');
+    var collection = db.collection(ACCOUNT);
     collection.findOne({},{sort:[["id","desc"]]},function(err,tweet){
       var since_id;
       if (!err){
         if (tweet){
           since_id = tweet.id_str;
-          console.log('Set "since_id" to ',since_id);
+          console.log('Set "since_id" for account '+ACCOUNT+' to ',since_id);
         }else{
           console.log('Cant find any tweet in database. Set "since_id" to "undefined".');
         }
@@ -129,7 +142,7 @@ function getResentTweets(){
       db.close();
 
       var params = {
-        screen_name: 'UberFacts',
+        screen_name: ACCOUNT,
         count: 200,
         include_rts: false,
         exclude_replies: true,
@@ -142,12 +155,12 @@ function getResentTweets(){
             tweets.pop();
           }
           if (tweets.length > 0){
-            console.log(tweets.length+' tweets from '+tweets[0].id_str+' to '+tweets[tweets.length-1].id_str+' where found since '+since_id);
+            console.log(tweets.length+' tweets from '+tweets[0].id_str+' to '+tweets[tweets.length-1].id_str+' for account '+ACCOUNT+' where found since '+since_id);
             //since_id = tweets[0].id_str;
             //console.log('Update "since" value to ',since_id);
             saveTweets(tweets);
           }else{
-            console.log('No new tweets were found since ',since_id);
+            console.log('No new tweets  for account '+ACCOUNT+' were found since ',since_id);
           }
         }else{
           console.log(err);
--- db_init.js
+++ db_init.js
@@ -1,6 +1,7 @@
 var Twitter = require('twitter');
 var MongoClient = require('mongodb').MongoClient;
 var translate = require('yandex-translate');
+var Q = require('q');
 
 var client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -9,15 +10,28 @@ var client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-initialLoad();
+function loadAccounts(accounts){
+  if (accounts && accounts.length>0){
+    var acc = accounts.shift();
+    resetCollection(acc).done(function(){
+      initialLoad(acc).done(function(){
+        loadAccounts(accounts);
+      });  
+    });
+  }else{
+    console.log('Accounts were loaded.')
+  }
+};
 
-function initialLoad(successHandler,errorHandler){
+loadAccounts(['5faktov','best_fact','hot_fact','factroom']);
 
+function initialLoad(account,successHandler,errorHandler){
+  var d = Q.defer();
   var count=16; //3200 of tweets
   var total = 0;
 
   var params = {
-    screen_name: 'UberFacts',
+    screen_name: account,
     count: 200,
     include_rts: false,
     exclude_replies: true
@@ -27,30 +41,59 @@ function initialLoad(successHandler,errorHandler){
     if (!error){
       total += tweets.length;
       console.log('Get '+tweets.length+' tweets from '+tweets[0].id_str+' to '+tweets[tweets.length-1].id_str+'. Total: '+total);
-      saveTweets(tweets,function(){
+      saveTweets(account,tweets,function(){
         if (count>0){
           count--;
           params.max_id = tweets[tweets.length-1].id_str;
           client.get('statuses/user_timeline.json', params,handler);
         }else{
-          if (successHandler) successHandler();
+          if (successHandler) {
+            successHandler();
+          }
+          d.resolve();              
         }
       });
     }else{
       console.log(error);
-      if (errorHandler) errorHandler(error);
+      if (errorHandler) {
+        errorHandler(error);
+      }
+      d.resolve();
     }
   };
+  console.log('Get tweets for account @'+account);
   client.get('statuses/user_timeline.json',params,handler);
+  return d.promise;
 }
 
-function saveTweets(tweets,successHandler,errorHandler){
+function resetCollection(collection){
+  var d = Q.defer();
+  MongoClient.connect(process.env.MONGO_URL, function(err, db) {
+    if(err) {
+      console.log(err);
+      d.reject();
+    }else{
+      db.dropCollection(collection,function(err){
+        if (err){
+          console.log(err);
+          d.reject();
+        }else{
+          console.log('Collection ['+collection+'] was cleared.');
+          d.resolve();
+        }
+      });
+    }
+  });
+  return d.promise;
+}
+
+function saveTweets(account,tweets,successHandler,errorHandler){
   MongoClient.connect(process.env.MONGO_URL, function(err, db) {
     if(err) {
       console.log(err);
       if (errorHandler) errorHandler(err);
     }
-    var collection = db.collection('tweets');
+    var collection = db.collection(account);
     var index = 0;
     var handler = function(err,docs){
       if (err) {
@@ -61,6 +104,10 @@ function saveTweets(tweets,successHandler,errorHandler){
         index++;
         var tweet = tweets[index];
         tweet._id = tweet.id_str;
+        tweet.status = 'N';
+        //tweet.text = tweet.text.replace(/(\r\n|\n|\r)/gm," ").replace(/\s+/," ");
+        collection.save(tweet,handler);
+        /*
         translate(tweet.text, { to: 'ru', key: process.env.YANDEX_TRANSLATE_API_KEY }, function(err, res){
           if (!err){
             tweet.text_ru = res.text[0];
@@ -69,6 +116,7 @@ function saveTweets(tweets,successHandler,errorHandler){
           }
           collection.save(tweet,handler);
         });
+        */
       }else{
         db.close();
         successHandler();
